13c9597b88775324f71e93fb01f0bda0f18e7820
BluTargaryan <opeyemiyolo@gmail.com>
2025-07-31 21:06:53 +0100

    removed sensitive values

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBVjwst13AOvs8XqPZ890kcLVTHkSJyMGs",
-  authDomain: "offering-system-6e9b7.firebaseapp.com",
-  projectId: "offering-system-6e9b7",
-  storageBucket: "offering-system-6e9b7.appspot.com",
-  messagingSenderId: "410597421009",
-  appId: "1:410597421009:web:2e4c8c77cda781a10d7af0",
-  measurementId: "G-2DE5DVDK5D"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
